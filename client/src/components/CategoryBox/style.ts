c1b5e3326ab88bd7eaa2e5c06649010dbd685cb9
hasunghwa <tjdghk3920@naver.com>
2022-07-15 17:43:32 +0900

    Style : CategoryBox 스타일 적용

---
+++ client/src/components/CategoryBox/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const CategoryBoxWrapper = styled.div`
+  display: grid;
+  grid: '. . .';
+  row-gap: 50px;
+  column-gap: 50px;
+  width: 70%;
+`;
+
+export { CategoryBoxWrapper };
