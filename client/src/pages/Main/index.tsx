6e44a706bfca59a93c1620d6066f4dc0d0564152
hasunghwa <tjdghk3920@naver.com>
2022-07-15 15:27:10 +0900

    Style: 메인 페이지에 기본 Layout 적용

---
+++ client/src/pages/Main/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+function Main() {
+  return <Layout>메인화면</Layout>;
+}
+
+export default Main;
---
+++ client/src/pages/Main/style.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import color from '../../styles/Colors';
+
+const MainWarapper = styled.div``;
+
+export { MainWarapper };
