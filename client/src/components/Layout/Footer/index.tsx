f3217f3e69a888d8c7bef9381180ab6c6fb0476e
hasunghwa <tjdghk3920@naver.com>
2022-07-15 15:12:24 +0900

    Style : Footer 레이아웃 생성

---
+++ client/src/components/Layout/Footer/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { FooterWarapper } from './style';
+
+const Footer = () => {
+  return <FooterWarapper></FooterWarapper>;
+};
+
+export default Footer;
---
+++ client/src/components/Layout/Footer/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import color from '../../styles/Colors';
+
+const FooterWarapper = styled.footer`
+  background-color: #aaaaaa;
+  height: 75px;
+  margin-top: auto;
+`;
+
+export { FooterWarapper };
