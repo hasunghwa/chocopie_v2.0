4869e5faff59a64049cfb1d40cbe814cfd2c5bbd
hasunghwa <tjdghk3920@naver.com>
2022-07-16 20:35:31 +0900

    Fix : CategoryBox grid 스타일 수정
    justify-content를 추가하여 item 가운데로 정렬

--- client/src/components/CategoryBox/style.ts
+++ client/src/components/CategoryBox/style.ts
@@ -2,10 +2,9 @@ import styled from 'styled-components';
 
 const CategoryBoxWrapper = styled.div`
   display: grid;
-  grid: '. . .';
-  row-gap: 50px;
-  column-gap: 50px;
-  width: 70%;
+  grid-template-columns: repeat(3, minmax(300px, auto));
+  justify-content: center;
+  gap: 50px 30px;
 `;
 
 export { CategoryBoxWrapper };
