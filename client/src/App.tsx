6242ba62ef184536774e2cf1814a7630c2349c78
hasunghwa <tjdghk3920@naver.com>
2022-07-17 21:36:49 +0900

    Feature: Main 페이지 Router 연동

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
-function App() {
-  const [count, setCount] = useState(0)
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Main from './pages/Main';
 import GlobalStyle from './styles/GlobalStyle';
 
+function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
