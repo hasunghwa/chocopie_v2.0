bffdff9e407ecbbcc9abd26807981417b5e39a8d
hasunghwa <tjdghk3920@naver.com>
2022-07-18 21:14:19 +0900

    Feature : storelist 페이지 Dom 연동

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Main from '@pages/Main';
 import GlobalStyle from '@styles/GlobalStyle';
+import StoreList from '@pages/StoreList';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/storelist/:category" element={<StoreList />} />
       </Routes>
     </div>
   );
