0fab6e7890cf637f145ce0d1547106d02b5a972a
hasunghwa <tjdghk3920@naver.com>
2022-07-19 17:19:41 +0900

    Feature : Storeinfo Side 구현

---
+++ client/src/components/Layout/StoreInfo/Side/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import {
+  StoreinfoSideWrapper,
+  StoreImg,
+  StoreName,
+  WriteBtn,
+  SettingBtn,
+} from './style';
+
+interface StoreProps {
+  id: number;
+  name: string;
+  mainImg: string;
+  introduce: string;
+  owner: string;
+  address: string;
+  phone: string;
+}
+
+const StoreinfoSide = () => {
+  const StoreList: StoreProps = {
+    id: 1,
+    name: '가게1',
+    mainImg:
+      'https://lh3.googleusercontent.com/1nwmAGqHnWvC7sWwqaq-GV2kr0e9tnCkzlc1u_oPG0R29DUSVMAYCQcjEA8b01Hsaw5jedhuI7LCIG6baFWuyReLOw3NM68jD_00qG0tNky5l6WXhIkyDc-4SMBTn35UFUgfCj9uIZAsJjN7jeh1b_MnQzFRS5MIIzCbXXRmoDwgA__CWtV3b8bhHFmat1wRTYWSitsOk4G-hNvzzVTFuYk7eSF5tXK6zPd6BFXgVL7FwQrJma4lumuWMJrZo1zT0mZ3hEcxU_IZM8HcnahsBdo2qnGS66ZA9tyBIuBQKsJBtKXtrX2jcuaKog38pWELtsbIVdY69uT7Mw0lTbaabxjFav_MZddUx6VFC9IKpo0qPLiAHCURbcuOX9ydYrXUNlP5dZu5EwcA_YbroWSTRcsLocS9FTjsrvnA0INMrPEYWjPO1-GB3v0LkidfXlrUEFWB0enQAukgs-OuxxQwqixBIDz5Hq0-OHFdyuXDM-u9Gx5XacFXDoOjTmf5ImArDioYriHh86gvT6LiO8SreAQXQp9p_Xw2YQ-o6ENTGz6Gp8MmG_nozArqMdfw64QE3lmOfehZ8b1OHpLjAFa2pK7A96QAB_2hya17jziLxv8h_eIMZiMULf6TtDHSNlN7J79PUtMjK7RxHVCw23_JxeUMIoglRXvfcQCj2v6usSrpIU3_6-QnvNNREDAxgu5v0JsvJWnaAUKXCO31aY4fvQVVFFP6xeAKrkuBnXws0xyX7uaTLV1yW-0Iux5kN5tzCsWutM_PI3SA1dQlFAgMjlbYK1VPvDuEnw=w1320-h990-no?authuser=1',
+    introduce: '팔아요',
+    owner: '나다',
+    address: '지구 어딘가',
+    phone: '010-3333-3333',
+  };
+
+  return (
+    <StoreinfoSideWrapper>
+      <StoreImg src={StoreList.mainImg} />
+      <StoreName>
+        <h3>{StoreList.name}</h3>
+        <p>{StoreList.introduce}</p>
+      </StoreName>
+      <WriteBtn>글쓰기</WriteBtn>
+      {/* 밴드 주인이면 */}
+      <SettingBtn>밴드 설정</SettingBtn>
+    </StoreinfoSideWrapper>
+  );
+};
+
+export default StoreinfoSide;
