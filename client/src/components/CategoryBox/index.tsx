7017f968ba6e22f4cfd1a69e2933a31e0020ff91
hasunghwa <tjdghk3920@naver.com>
2022-07-21 20:25:49 +0900

    Fix : 이미지 상대경로로 인한 로드 오류 수정

--- client/src/components/CategoryBox/index.tsx
+++ client/src/components/CategoryBox/index.tsx
@@ -11,32 +11,32 @@ interface CategoryProps {
 const CategoryBox = () => {
   const Categorys: CategoryProps[] = [
     {
-      imgPath: './img/category-img1.jpg',
+      imgPath: '/img/category-img1.jpg',
       linkTo: '/storelist/agricultural',
       categoryName: '농산물',
     },
     {
-      imgPath: './img/category-img2.jpg',
+      imgPath: '/img/category-img2.jpg',
       linkTo: '/storelist/seafood',
       categoryName: '수산물',
     },
     {
-      imgPath: './img/category-img3.jpg',
+      imgPath: '/img/category-img3.jpg',
       linkTo: '/storelist/meat',
       categoryName: '축산물',
     },
     {
-      imgPath: './img/category-img4.jpg',
+      imgPath: '/img/category-img4.jpg',
       linkTo: '/storelist/sidedish',
       categoryName: '반찬',
     },
     {
-      imgPath: './img/category-img5.jpg',
+      imgPath: '/img/category-img5.jpg',
       linkTo: '/storelist/clothing',
       categoryName: '의류',
     },
     {
-      imgPath: './img/category-img6.jpg',
+      imgPath: '/img/category-img6.jpg',
       linkTo: '/storelist/health',
       categoryName: '건강식품',
     },
