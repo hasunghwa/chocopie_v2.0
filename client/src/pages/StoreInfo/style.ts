42936e00064455c2ff4bac5fc1afa48ab4eb3b56
hasunghwa <tjdghk3920@naver.com>
2022-07-24 21:09:22 +0900

    Style : StoreInfo 페이지 스타일 적용

---
+++ client/src/pages/StoreInfo/style.ts
@@ -0,0 +1,15 @@
+import color from '@styles/Colors';
+import styled from 'styled-components';
+
+const SearchForm = styled.form`
+  box-shadow: 0 1.4px 1.3px 0 rgb(62 73 89 / 10%);
+  background-color: ${color.White};
+  margin-bottom: 12px;
+  padding: 0 45px 0 15px;
+  & input {
+    width: 100%;
+    height: 40px;
+  }
+`;
+
+export { SearchForm };
