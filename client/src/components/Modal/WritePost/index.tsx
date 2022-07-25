1927dc633eb8b23c329bfffae147f85c921af0d0
hasunghwa <tjdghk3920@naver.com>
2022-07-25 21:40:05 +0900

    Feature : WritePost Modal 구현
    글쓰기 모달 구현

--- client/src/components/Modal/WritePost/index.tsx
+++ client/src/components/Modal/WritePost/index.tsx
@@ -1,22 +1,34 @@
 import React from 'react';
 
-import Modal from '..';
+import Editor from '@components/Editor';
+import Colors from '@styles/Colors';
 
-interface ModalController {
-  hide: () => void;
-  show?: () => void;
-}
+import Modal from '..';
+import { WritePostWrapper } from './style';
 
 function WritePostModal({ controller }: { controller: ModalController }) {
   const middleContent = (
-    <>
-      <div>아</div>
-    </>
+    <WritePostWrapper>
+      <Editor />
+    </WritePostWrapper>
   );
+
+  const writePost = () => {
+    console.log('게시');
+  };
+
   return (
     <Modal
       props={{
         middleContent: middleContent,
+        title: '글쓰기',
+        bottomRightButton: {
+          text: '게시',
+          color: Colors.Blue,
+          onClickHandler: () => {
+            writePost();
+          },
+        },
       }}
       controller={controller}
     />
