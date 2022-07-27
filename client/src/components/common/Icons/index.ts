4c6bdef90a7c2b9f4b8ba48068d14245cf3a164f
hasunghwa <tjdghk3920@naver.com>
2022-07-27 17:29:36 +0900

    Chore: svg를 ReactComponent로 사용하기 위한 설정

---
+++ client/src/components/common/Icons/index.ts
@@ -0,0 +1,4 @@
+import { ReactComponent as MarkerIcon } from '@assets/icons/ic-marker.svg';
+import { ReactComponent as CancelIcon } from '@assets/icons/ic-cancel.svg';
+
+export { MarkerIcon, CancelIcon };
