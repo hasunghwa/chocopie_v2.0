9233c9fdf450b0853a6fb14fa4f215a4b4135885
hasunghwa <tjdghk3920@naver.com>
2022-07-27 17:25:35 +0900

    Style : 검정색 연한회색 색상 추가

--- client/src/styles/Colors.ts
+++ client/src/styles/Colors.ts
@@ -3,7 +3,9 @@ enum Colors {
   LightRed = '#FFC4C4',
   Red = '#A10035',
   White = '#FFFFFF',
+  Black = '#000000',
   LightGray = '#EEEEEE',
+  LightGray2 = '#ebebeb',
   DarkGray = '#787878',
 }
 
