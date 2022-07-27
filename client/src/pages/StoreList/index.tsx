e421abdaedf654d1e356d82b9093d51418042399
hasunghwa <tjdghk3920@naver.com>
2022-07-27 17:35:13 +0900

    Fix : 임시코드 삭제

--- client/src/pages/StoreList/index.tsx
+++ client/src/pages/StoreList/index.tsx
@@ -1,28 +1,13 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
 
 import Layout from '@components/Layout';
 import Stores from '@components/Stores';
 import { StoreListWrapper } from './style';
 
-enum Category {
-  agricultural = '농산물',
-  seafood = '수산물',
-  meat = '축산물',
-  sidedish = '반찬',
-  clothing = '의류',
-  health = '건강식품',
-}
-
-type CategoryStrings = keyof typeof Category;
-
 function StoreList() {
-  const selectCategory = useParams().category as CategoryStrings;
-
   return (
     <Layout isFooter={false}>
       <StoreListWrapper>
-        {Category[selectCategory]}
         <Stores />
       </StoreListWrapper>
     </Layout>
