90484d749c004ad15ab316b8713f85733aff4379
hasunghwa <tjdghk3920@naver.com>
2022-07-29 16:24:06 +0900

    Style: PostCode component 스타일 적용

---
+++ client/src/components/KaKaoMap/PostCode/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import Colors from '@styles/Colors';
+
+const Button = styled.button`
+  position: absolute;
+  left: 20px;
+  bottom: 16px;
+  padding: 8px 16px;
+  background-color: ${Colors.LightRed};
+`;
+
+export { Button };
