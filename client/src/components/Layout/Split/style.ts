c7c2e5c14194669e5e502e247893e7550611bced
hasunghwa <tjdghk3920@naver.com>
2022-07-31 19:26:39 +0900

    Style : Split Layout 스타일 적용

---
+++ client/src/components/Layout/Split/style.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const SplitWrapper = styled.div`
+  display: flex;
+  width: 1024px;
+`;
+
+const LeftContent = styled.div``;
+
+const CenterContent = styled.div`
+  margin-left: 20px;
+`;
+
+const RightContent = styled.div`
+  margin-left: 20px;
+`;
+
+export { SplitWrapper, LeftContent, CenterContent, RightContent };
