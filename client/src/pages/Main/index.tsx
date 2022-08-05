123b7243eff70a95883f79071f58a251e84ba057
hasunghwa <tjdghk3920@naver.com>
2022-08-05 20:17:32 +0900

    Featurex : Main 페이지 시장선택 기능 연동 및 스타일 적용
    시장선택 모달 적용

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Layout from '@components/Layout';
 import CategoryBox from '@components/CategoryBox';
+import MapModal from '@components/Modal/Map';
+import { MainWrapper, MarketSelectBtn } from './style';
+import { MarkerIcon } from '@components/common/Icons';
 
 function Main() {
+  const [openMap, setOpenMap] = useState(false);
+
+  const mapModalControl: ModalController = {
+    hide: () => setOpenMap(false),
+    show: () => setOpenMap(true),
+  };
+
   return (
-    <Layout>
-      <CategoryBox></CategoryBox>
-    </Layout>
+    <>
+      <Layout>
+        <MainWrapper>
+          <MarketSelectBtn onClick={() => setOpenMap(!openMap)}>
+            <MarkerIcon width="21" height="24" />
+            <span>시장 선택하러 가기</span>
+          </MarketSelectBtn>
+          <CategoryBox />
+        </MainWrapper>
+      </Layout>
+      {openMap && <MapModal controller={mapModalControl} />}
+    </>
   );
 }
 
--- client/src/pages/Main/style.ts
+++ client/src/pages/Main/style.ts
@@ -1,6 +1,25 @@
 import styled from 'styled-components';
 import color from '../../styles/Colors';
 
-const MainWrapper = styled.div``;
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 600px;
+  gap: 30px;
+`;
 
-export { MainWrapper };
+const MarketSelectBtn = styled.button`
+  min-height: 56px;
+  width: 100%;
+  height: auto;
+  padding: 14px 30px;
+  background-color: ${color.White};
+  box-shadow: 0 1.4px 1.3px 0 rgb(62 73 89 / 10%);
+
+  span {
+    font-size: 16px;
+  }
+`;
+
+export { MainWrapper, MarketSelectBtn };
