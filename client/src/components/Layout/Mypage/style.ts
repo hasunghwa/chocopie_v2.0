ddfc2a06cff636079f00111219c470e005d06ff9
hasunghwa <tjdghk3920@naver.com>
2022-08-07 20:36:39 +0900

    Style :  Mypage Layout 스타일 적용

---
+++ client/src/components/Layout/Mypage/style.ts
@@ -0,0 +1,20 @@
+import Colors from '@styles/Colors';
+import styled from 'styled-components';
+
+const StoreInfoWrapper = styled.div`
+  display: flex;
+  width: 1024px;
+  margin: 0 auto;
+  height: 600px;
+`;
+
+const StoreInfoContent = styled.div`
+  z-index: 50;
+  width: 720px;
+  //margin: 0px 230px;
+  background-color: ${Colors.White};
+`;
+
+const StoreInfoempty = styled.div``;
+
+export { StoreInfoWrapper, StoreInfoContent, StoreInfoempty };
