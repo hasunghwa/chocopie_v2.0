a7f3b7b1c3aacea61b073356e26684510d6c6c9f
DESKTOP-ICDGUQJ\sunghwa <tjdghk3920@naver.com>
2022-08-20 00:41:23 +0900

    Feature : 주문 컴포넌트 구현

---
+++ client/src/components/My/Order/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { OrderWrapper } from './style';
+
+import OrderTable from './Table';
+
+const Order = () => {
+  return (
+    <OrderWrapper>
+      <OrderTable />
+    </OrderWrapper>
+  );
+};
+
+export default Order;
