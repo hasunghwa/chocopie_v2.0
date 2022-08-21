3d5fc91e8418e2f56b94dae07510a454491f57e9
DESKTOP-ICDGUQJ\sunghwa <tjdghk3920@naver.com>
2022-08-21 20:29:30 +0900

    Style : Map modal 스타일 적용

---
+++ client/src/components/Modal/Map/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const MapWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 560px;
+  height: 400px;
+`;
+
+export { MapWrapper };
