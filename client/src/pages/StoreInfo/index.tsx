9dfde41ea43226ace4ced828a4347c99e1fcc928
DESKTOP-ICDGUQJ\sunghwa <tjdghk3920@naver.com>
2022-09-14 18:05:12 +0900

    Refactor : StoreInfo SplitLayout 적용
    기존의 StoreInfoLayout 에서 확장성을 위해 SplitLayout으로 변경

--- client/src/pages/StoreInfo/index.tsx
+++ client/src/pages/StoreInfo/index.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 
-import StoreInfoLayout from '@components/Layout/StoreInfo';
 import Board from '@components/Board';
-import { SearchForm } from './style';
+import { SearchForm, CenterContent } from './style';
+import StoreinfoSide from '@components/Side/Storeinfo';
+import SplitLayout from '@components/Layout/Split';
 
 const StoreInfo = () => {
   return (
-    <StoreInfoLayout>
-      <SearchForm>
-        <input placeholder="게시글 검색" type="text"></input>
-      </SearchForm>
-      <Board></Board>
-      <Board></Board>
-      <Board></Board>
-      <Board></Board>
-      <Board></Board>
-      <Board></Board>
-    </StoreInfoLayout>
+    <SplitLayout>
+      <StoreinfoSide />
+      <CenterContent>
+        <SearchForm>
+          <input placeholder="게시글 검색" type="text"></input>
+        </SearchForm>
+        <Board></Board>
+        <Board></Board>
+        <Board></Board>
+        <Board></Board>
+        <Board></Board>
+        <Board></Board>
+      </CenterContent>
+    </SplitLayout>
   );
 };
 
