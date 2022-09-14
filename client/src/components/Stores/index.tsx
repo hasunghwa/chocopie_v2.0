388209aea1b22c3e49f48978212542504d17956c
DESKTOP-ICDGUQJ\sunghwa <tjdghk3920@naver.com>
2022-09-14 18:06:50 +0900

    Refactor : 가게 이미지 파일 변경

--- client/src/components/Stores/index.tsx
+++ client/src/components/Stores/index.tsx
@@ -19,7 +19,7 @@ const Stores = () => {
       id: 1,
       name: '가게1',
       mainImg:
-        'https://lh3.googleusercontent.com/1nwmAGqHnWvC7sWwqaq-GV2kr0e9tnCkzlc1u_oPG0R29DUSVMAYCQcjEA8b01Hsaw5jedhuI7LCIG6baFWuyReLOw3NM68jD_00qG0tNky5l6WXhIkyDc-4SMBTn35UFUgfCj9uIZAsJjN7jeh1b_MnQzFRS5MIIzCbXXRmoDwgA__CWtV3b8bhHFmat1wRTYWSitsOk4G-hNvzzVTFuYk7eSF5tXK6zPd6BFXgVL7FwQrJma4lumuWMJrZo1zT0mZ3hEcxU_IZM8HcnahsBdo2qnGS66ZA9tyBIuBQKsJBtKXtrX2jcuaKog38pWELtsbIVdY69uT7Mw0lTbaabxjFav_MZddUx6VFC9IKpo0qPLiAHCURbcuOX9ydYrXUNlP5dZu5EwcA_YbroWSTRcsLocS9FTjsrvnA0INMrPEYWjPO1-GB3v0LkidfXlrUEFWB0enQAukgs-OuxxQwqixBIDz5Hq0-OHFdyuXDM-u9Gx5XacFXDoOjTmf5ImArDioYriHh86gvT6LiO8SreAQXQp9p_Xw2YQ-o6ENTGz6Gp8MmG_nozArqMdfw64QE3lmOfehZ8b1OHpLjAFa2pK7A96QAB_2hya17jziLxv8h_eIMZiMULf6TtDHSNlN7J79PUtMjK7RxHVCw23_JxeUMIoglRXvfcQCj2v6usSrpIU3_6-QnvNNREDAxgu5v0JsvJWnaAUKXCO31aY4fvQVVFFP6xeAKrkuBnXws0xyX7uaTLV1yW-0Iux5kN5tzCsWutM_PI3SA1dQlFAgMjlbYK1VPvDuEnw=w1320-h990-no?authuser=1',
+        'https://images.unsplash.com/photo-1580854256166-e01d8a34da4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI2fHhqUFI0aGxrQkdBfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
       introduce: '팔아요',
       owner: '나다',
       address: '지구 어딘가',
@@ -29,7 +29,7 @@ const Stores = () => {
       id: 2,
       name: '가게2',
       mainImg:
-        'https://lh3.googleusercontent.com/1nwmAGqHnWvC7sWwqaq-GV2kr0e9tnCkzlc1u_oPG0R29DUSVMAYCQcjEA8b01Hsaw5jedhuI7LCIG6baFWuyReLOw3NM68jD_00qG0tNky5l6WXhIkyDc-4SMBTn35UFUgfCj9uIZAsJjN7jeh1b_MnQzFRS5MIIzCbXXRmoDwgA__CWtV3b8bhHFmat1wRTYWSitsOk4G-hNvzzVTFuYk7eSF5tXK6zPd6BFXgVL7FwQrJma4lumuWMJrZo1zT0mZ3hEcxU_IZM8HcnahsBdo2qnGS66ZA9tyBIuBQKsJBtKXtrX2jcuaKog38pWELtsbIVdY69uT7Mw0lTbaabxjFav_MZddUx6VFC9IKpo0qPLiAHCURbcuOX9ydYrXUNlP5dZu5EwcA_YbroWSTRcsLocS9FTjsrvnA0INMrPEYWjPO1-GB3v0LkidfXlrUEFWB0enQAukgs-OuxxQwqixBIDz5Hq0-OHFdyuXDM-u9Gx5XacFXDoOjTmf5ImArDioYriHh86gvT6LiO8SreAQXQp9p_Xw2YQ-o6ENTGz6Gp8MmG_nozArqMdfw64QE3lmOfehZ8b1OHpLjAFa2pK7A96QAB_2hya17jziLxv8h_eIMZiMULf6TtDHSNlN7J79PUtMjK7RxHVCw23_JxeUMIoglRXvfcQCj2v6usSrpIU3_6-QnvNNREDAxgu5v0JsvJWnaAUKXCO31aY4fvQVVFFP6xeAKrkuBnXws0xyX7uaTLV1yW-0Iux5kN5tzCsWutM_PI3SA1dQlFAgMjlbYK1VPvDuEnw=w1320-h990-no?authuser=1',
+        'https://images.unsplash.com/photo-1580854256166-e01d8a34da4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI2fHhqUFI0aGxrQkdBfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
       introduce: '팔아요',
       owner: '나다',
       address: '지구 어딘가',
@@ -39,7 +39,7 @@ const Stores = () => {
       id: 3,
       name: '가게3',
       mainImg:
-        'https://lh3.googleusercontent.com/1nwmAGqHnWvC7sWwqaq-GV2kr0e9tnCkzlc1u_oPG0R29DUSVMAYCQcjEA8b01Hsaw5jedhuI7LCIG6baFWuyReLOw3NM68jD_00qG0tNky5l6WXhIkyDc-4SMBTn35UFUgfCj9uIZAsJjN7jeh1b_MnQzFRS5MIIzCbXXRmoDwgA__CWtV3b8bhHFmat1wRTYWSitsOk4G-hNvzzVTFuYk7eSF5tXK6zPd6BFXgVL7FwQrJma4lumuWMJrZo1zT0mZ3hEcxU_IZM8HcnahsBdo2qnGS66ZA9tyBIuBQKsJBtKXtrX2jcuaKog38pWELtsbIVdY69uT7Mw0lTbaabxjFav_MZddUx6VFC9IKpo0qPLiAHCURbcuOX9ydYrXUNlP5dZu5EwcA_YbroWSTRcsLocS9FTjsrvnA0INMrPEYWjPO1-GB3v0LkidfXlrUEFWB0enQAukgs-OuxxQwqixBIDz5Hq0-OHFdyuXDM-u9Gx5XacFXDoOjTmf5ImArDioYriHh86gvT6LiO8SreAQXQp9p_Xw2YQ-o6ENTGz6Gp8MmG_nozArqMdfw64QE3lmOfehZ8b1OHpLjAFa2pK7A96QAB_2hya17jziLxv8h_eIMZiMULf6TtDHSNlN7J79PUtMjK7RxHVCw23_JxeUMIoglRXvfcQCj2v6usSrpIU3_6-QnvNNREDAxgu5v0JsvJWnaAUKXCO31aY4fvQVVFFP6xeAKrkuBnXws0xyX7uaTLV1yW-0Iux5kN5tzCsWutM_PI3SA1dQlFAgMjlbYK1VPvDuEnw=w1320-h990-no?authuser=1',
+        'https://images.unsplash.com/photo-1580854256166-e01d8a34da4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI2fHhqUFI0aGxrQkdBfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
       introduce: '팔아요',
       owner: '나다',
       address: '지구 어딘가',
@@ -49,7 +49,7 @@ const Stores = () => {
       id: 4,
       name: '가게4',
       mainImg:
-        'https://lh3.googleusercontent.com/1nwmAGqHnWvC7sWwqaq-GV2kr0e9tnCkzlc1u_oPG0R29DUSVMAYCQcjEA8b01Hsaw5jedhuI7LCIG6baFWuyReLOw3NM68jD_00qG0tNky5l6WXhIkyDc-4SMBTn35UFUgfCj9uIZAsJjN7jeh1b_MnQzFRS5MIIzCbXXRmoDwgA__CWtV3b8bhHFmat1wRTYWSitsOk4G-hNvzzVTFuYk7eSF5tXK6zPd6BFXgVL7FwQrJma4lumuWMJrZo1zT0mZ3hEcxU_IZM8HcnahsBdo2qnGS66ZA9tyBIuBQKsJBtKXtrX2jcuaKog38pWELtsbIVdY69uT7Mw0lTbaabxjFav_MZddUx6VFC9IKpo0qPLiAHCURbcuOX9ydYrXUNlP5dZu5EwcA_YbroWSTRcsLocS9FTjsrvnA0INMrPEYWjPO1-GB3v0LkidfXlrUEFWB0enQAukgs-OuxxQwqixBIDz5Hq0-OHFdyuXDM-u9Gx5XacFXDoOjTmf5ImArDioYriHh86gvT6LiO8SreAQXQp9p_Xw2YQ-o6ENTGz6Gp8MmG_nozArqMdfw64QE3lmOfehZ8b1OHpLjAFa2pK7A96QAB_2hya17jziLxv8h_eIMZiMULf6TtDHSNlN7J79PUtMjK7RxHVCw23_JxeUMIoglRXvfcQCj2v6usSrpIU3_6-QnvNNREDAxgu5v0JsvJWnaAUKXCO31aY4fvQVVFFP6xeAKrkuBnXws0xyX7uaTLV1yW-0Iux5kN5tzCsWutM_PI3SA1dQlFAgMjlbYK1VPvDuEnw=w1320-h990-no?authuser=1',
+        'https://images.unsplash.com/photo-1580854256166-e01d8a34da4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI2fHhqUFI0aGxrQkdBfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
       introduce: '팔아요',
       owner: '나다',
       address: '지구 어딘가',
@@ -59,7 +59,7 @@ const Stores = () => {
       id: 5,
       name: '가게5',
       mainImg:
-        'https://lh3.googleusercontent.com/1nwmAGqHnWvC7sWwqaq-GV2kr0e9tnCkzlc1u_oPG0R29DUSVMAYCQcjEA8b01Hsaw5jedhuI7LCIG6baFWuyReLOw3NM68jD_00qG0tNky5l6WXhIkyDc-4SMBTn35UFUgfCj9uIZAsJjN7jeh1b_MnQzFRS5MIIzCbXXRmoDwgA__CWtV3b8bhHFmat1wRTYWSitsOk4G-hNvzzVTFuYk7eSF5tXK6zPd6BFXgVL7FwQrJma4lumuWMJrZo1zT0mZ3hEcxU_IZM8HcnahsBdo2qnGS66ZA9tyBIuBQKsJBtKXtrX2jcuaKog38pWELtsbIVdY69uT7Mw0lTbaabxjFav_MZddUx6VFC9IKpo0qPLiAHCURbcuOX9ydYrXUNlP5dZu5EwcA_YbroWSTRcsLocS9FTjsrvnA0INMrPEYWjPO1-GB3v0LkidfXlrUEFWB0enQAukgs-OuxxQwqixBIDz5Hq0-OHFdyuXDM-u9Gx5XacFXDoOjTmf5ImArDioYriHh86gvT6LiO8SreAQXQp9p_Xw2YQ-o6ENTGz6Gp8MmG_nozArqMdfw64QE3lmOfehZ8b1OHpLjAFa2pK7A96QAB_2hya17jziLxv8h_eIMZiMULf6TtDHSNlN7J79PUtMjK7RxHVCw23_JxeUMIoglRXvfcQCj2v6usSrpIU3_6-QnvNNREDAxgu5v0JsvJWnaAUKXCO31aY4fvQVVFFP6xeAKrkuBnXws0xyX7uaTLV1yW-0Iux5kN5tzCsWutM_PI3SA1dQlFAgMjlbYK1VPvDuEnw=w1320-h990-no?authuser=1',
+        'https://images.unsplash.com/photo-1580854256166-e01d8a34da4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI2fHhqUFI0aGxrQkdBfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
       introduce: '팔아요',
       owner: '나다',
       address: '지구 어딘가',
