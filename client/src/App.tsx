8ac83bcc073900decffb92e2d960454973a15d5e
DESKTOP-ICDGUQJ\sunghwa <tjdghk3920@naver.com>
2022-12-06 19:32:20 +0900

    Refactor : url path 상수화, lazy를 통한 코드문할

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import GlobalStyle from '@styles/GlobalStyle';
-import Main from '@pages/Main';
-import MyPage from '@pages/MyPage';
-import StoreList from '@pages/StoreList';
-import StoreInfo from '@pages/StoreInfo';
+
+const Main = lazy(() => import('@pages/Main'));
+const Login = lazy(() => import('@pages/Login'));
+const OauthRedirect = lazy(() => import('@pages/OauthRedirect'));
+const MyPage = lazy(() => import('@pages/MyPage'));
+const StoreList = lazy(() => import('@pages/StoreList'));
+const StoreInfo = lazy(() => import('@pages/StoreInfo'));
+
+const Loading = lazy(() => import('@components/common/Loading'));
+
+export const PATH = {
+  MAIN: '/',
+  LOGIN: '/login',
+  GOOGLE_LOGIN_REDIRECT: '/login/redirect',
+  MYPAGE: '/my/:category',
+  STORE_LIST: '/storelist/:category',
+  STORE_INFO: '/storeinfo/:id',
+};
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/my/:category" element={<MyPage />} />
-        <Route path="/storelist/:category" element={<StoreList />} />
-        <Route path="/storeinfo/:id" element={<StoreInfo />} />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path={PATH.MAIN} element={<Main />} />
+          <Route path={PATH.LOGIN} element={<Login />} />
+          <Route
+            path={PATH.GOOGLE_LOGIN_REDIRECT}
+            element={<OauthRedirect />}
+          />
+          <Route path={PATH.MYPAGE} element={<MyPage />} />
+          <Route path={PATH.STORE_LIST} element={<StoreList />} />
+          <Route path={PATH.STORE_INFO} element={<StoreInfo />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
